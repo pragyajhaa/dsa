fccf323f63ff2ac1ab498936de1b562763fa2e67
pragyajhaa <pragyajha1908@gmail.com>
2025-06-28 14:30:51 +0530

    initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Standard C++ header for input/output
+using namespace std; // Allows using standard library names (like cout) directly
+
+int main() {
+    cout << "Hello, World!" << endl; // Print "Hello, World!" and a newline
+    return 0; // Indicates successful execution
+}
